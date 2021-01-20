5d708bec5c6a9fcb585ed81e931112e18dde75c8
Denis <den0015@yandex.ru>
2021-01-20 14:33:07 +0300

    Добавление графики — еще правки)

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,11 +1,11 @@
 // Main Navigation
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--opened');
 navToggle.classList.remove('main-nav__toggle--nojs');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', () => {
   navMain.classList.toggle('main-nav--opened');
 });
